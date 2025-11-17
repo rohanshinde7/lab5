9e3e80f4ddf1719b71e4e8a8fa24d46e38d93681
Rohan Shinde <rh428532@dal.ca>
2025-11-16 20:56:10 -0400

    Updated frontend

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ return (
   <div className="container text-center">
     <h1>Welcome to My Full-Stack App </h1>
     <p>This is a React app styled with Bootstrap 5.</p>
-    <p>{state}</p>
+    {state ? <p>{state.message}</p> : <p>Loading...</p>}
   </div>
 );
 }
